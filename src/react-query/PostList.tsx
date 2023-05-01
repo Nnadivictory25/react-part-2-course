0de610bb0f63dd680c9745fdab5df0658bd30068
Nnadi Victory <chikamagozi25@gmail.com>
2023-05-01 21:01:58 +0100

    params in react-query

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -6,13 +6,24 @@ import usePosts from './hooks/usePosts';
 
 
 const PostList = () => {
-  const { data: posts, isLoading, error} = usePosts()
+  const [userId, setUserId] = useState<number>()
+  const { data: posts, isLoading, error} = usePosts(userId)
 
   if (isLoading) return <p>Loading...</p>
-  
+
   if (error) return <p>{error.message}</p>;
 
   return (
+    <>
+      <select
+        onChange={(event) => setUserId(Number(event.target.value))}
+        value={userId}
+        className="form-select mb-3">
+        <option value="">Select user</option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+    </select>
     <ul className="list-group">
       {posts?.map((post) => (
         <li key={post.id} className="list-group-item">
@@ -20,6 +31,7 @@ const PostList = () => {
         </li>
       ))}
     </ul>
+    </>
   );
 };
 
