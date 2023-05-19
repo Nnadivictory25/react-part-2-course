4ee02e8ce222cc7ec4c9172722d3bf4e432e74d5
Nnadi Victory <chikamagozi25@gmail.com>
2023-05-19 12:34:28 +0100

    mutating data contd..

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,41 +1,54 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useRef } from 'react';
 import { Todo } from './hooks/useTodos';
 import axios from 'axios';
 
 const TodoForm = () => {
-	const addTodo = useMutation({
+	const queryClient = useQueryClient();
+	const addTodo = useMutation<Todo, Error, Todo>({
 		mutationFn: (todo: Todo) =>
 			axios
 				.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
-        .then((res) => res.data),
-    onSuccess: (savedTodo, newTodo) => { 
-      console.log(savedTodo);
-    }
+				.then((res) => res.data),
+		onSuccess: (savedTodo, newTodo) => {
+			queryClient.setQueryData<Todo[]>(['todos'], (todos) => [
+				savedTodo,
+				...(todos || []),
+			]);
+
+			if (ref.current) ref.current.value = '';
+		},
 	});
 	const ref = useRef<HTMLInputElement>(null);
 
 	return (
-		<form
-			className='row mb-3'
-			onSubmit={(e) => {
-				e.preventDefault();
+		<>
+			{addTodo.error && (
+				<div className='alert alert-danger'>{addTodo.error.message}</div>
+			)}
+			<form
+				className='row mb-3'
+				onSubmit={(e) => {
+					e.preventDefault();
 
-				if (ref.current && ref.current.value)
-					addTodo.mutate({
-						id: 0,
-						title: ref.current?.value,
-						completed: false,
-						userId: 1,
-					});
-			}}>
-			<div className='col'>
-				<input ref={ref} type='text' className='form-control' />
-			</div>
-			<div className='col'>
-				<button className='btn btn-primary'>Add</button>
-			</div>
-		</form>
+					if (ref.current && ref.current.value)
+						addTodo.mutate({
+							id: 0,
+							title: ref.current?.value,
+							completed: false,
+							userId: 1,
+						});
+				}}>
+				<div className='col'>
+					<input ref={ref} type='text' className='form-control' />
+				</div>
+				<div className='col'>
+					<button className='btn btn-primary'>
+						{addTodo.isLoading ? 'Adding...' : 'Add'}
+					</button>
+				</div>
+			</form>
+		</>
 	);
 };
 
