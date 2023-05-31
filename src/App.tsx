e0f46cb22e46b6fff424ab874320dab60f11fe95
Nnadi Victory <chikamagozi25@gmail.com>
2023-05-31 12:44:59 +0100

    use reducer intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import './App.css';
-import PostList from './react-query/PostList';
-import TodoForm from './react-query/TodoForm';
-import TodoList from './react-query/TodoList';
+import Counter from './state-management/Counter';
 
 function App() {
 	return (
-		<>
-			<TodoForm />
-			<TodoList />
-		</>
+		<Counter />
 	);
 }
 
