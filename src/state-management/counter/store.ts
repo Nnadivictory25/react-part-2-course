b611dfca67d2fcd998302fdbdd16adf2a644a9fa
Nnadi Victory <chikamagozi25@gmail.com>
2023-06-01 14:13:45 +0100

    zustand dev tools

--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -1,4 +1,5 @@
-import {create} from 'zustand'
+import { create } from 'zustand'
+import { mountStoreDevtool} from 'simple-zustand-devtools'
 
 
 interface CounterStore {
@@ -12,5 +13,9 @@ const useCounterStore = create<CounterStore>(set => ({
     increment: () => set(store => ({ count: store.count + 1 })),
     reset: () => set(() => ({ count: 0}))
 }))
+
+if (process.env.NODE_ENV === 'development') {
+    mountStoreDevtool('coCounter Store', useCounterStore)
+}
 
 export default useCounterStore;
