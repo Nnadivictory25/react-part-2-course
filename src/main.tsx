c9a4233d7de8abe5a6c0475e902820ef89fcf85a
Nnadi Victory <chikamagozi25@gmail.com>
2023-06-05 20:34:23 +0100

    react router intro

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import {RouterProvider} from 'react-router-dom'
 import App from './App';
 import './index.css';
+import router from './routing/routes';
 
 const queryClient = new QueryClient(); 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
       <ReactQueryDevtools />
     </QueryClientProvider>
 	</React.StrictMode>
