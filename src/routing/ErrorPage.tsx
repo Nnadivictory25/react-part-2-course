9d40bafe3ccdc12bb019df8e4df5f573d3effe79
Nnadi Victory <chikamagozi25@gmail.com>
2023-06-07 12:13:59 +0100

    handling errors

--- src/routing/ErrorPage.tsx
+++ src/routing/ErrorPage.tsx
@@ -1,10 +1,18 @@
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+
 const ErrorPage = () => {
-  return (
-    <>
-      <h1>Oops...</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-    </>
-  );
+	const error = useRouteError();
+
+	return (
+		<>
+			<h1>Oops...</h1>
+			<p>
+				{isRouteErrorResponse(error)
+					? 'Page not found'
+					: 'Sorry, an unexpected error has occurred.'}
+			</p>
+		</>
+	);
 };
 
 export default ErrorPage;
